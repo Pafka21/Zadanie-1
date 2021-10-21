e17a2a0b7a8865b4d92431001d19d47a8eae2376
Paweł Bojda <92651557+Pafka21@users.noreply.github.com>
2021-10-21 18:54:21 +0200

    Add files via upload

---
+++ zad1.cpp
@@ -0,0 +1,99 @@
+﻿// zad1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	/* zad 1
+	cout << "nr. albumu: 65457";
+	*/
+
+	/* zad 2
+	int a;
+	cout << "Podaj liczbe: \n";
+	cin >> a;
+
+	if (a%2==0)
+	{
+		cout << "liczba parzysta";
+	}
+	else
+	{
+		cout << "liczba nieparzysta";
+	}
+	*/
+
+	/* zad 3
+	float a, b;
+
+	cout << "Podaj dwie liczby: \n";
+	cin >> a;
+	cin >> b;
+
+	if (a==b)
+	{
+		cout << "Liczby sa rowne\n";
+	}
+	else if (a>b)
+	{
+		cout << "Liczba: "<<a<<" jest wieksza";
+	}
+	else
+	{
+		cout << "Liczba: "<<b<<" jest wieksza";
+	}
+	*/
+
+	/* zad 4
+	float a, b, c, d;
+	
+	cout << "Podaj 4 liczby: \n";
+	cin >> a >> b >> c >> d;
+
+	cout << "Średnia to: " << (a + b + c + d) / 4;
+	*/
+
+	/*
+	float a, b;
+	int wybor;
+	cout << "Podaj dwie liczby: ";
+	cin >> a >> b;
+	cout << " 1. Dodawanie\n 2. Odejmowanie\n 3. Mnozenie\n 4. Dzielenie\n";
+	cout << "Wybierz opcje:", cin >> wybor;
+
+	switch (wybor)
+	{
+	case 1:
+		cout << "Suma " << a << " + " << b << " = " << a + b << endl;
+		break;
+	case 2:
+		cout << "Roznica " << a << " - " << b << " = " << a - b << endl;
+		break;
+	case 3:
+		cout << "Iloczyn " << a << " * " << b << " = " << a + b << endl;
+		break;
+	case 4:
+		if (b != 0)
+			cout << "Iloraz " << a << " / " << b << " = " << a / b << endl;
+		else
+			cout << "Nie dziel przez 0";
+		break;
+	
+	default:
+		cout << "nie ma takiej opcji\n";
+		break;
+	}
+	*/
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
